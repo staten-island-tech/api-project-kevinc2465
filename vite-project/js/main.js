70621f7fddaf25ba03b958dc0c65099ed4bdbce3
kevinc2465 <chenk5104@gmail.com>
2023-01-10 13:13:29 -0500

    ???

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,1 +1,13 @@
 import "../styles/style.css";
+
+const URL = "https://api.punkapi.com/v2/beers/random";
+
+async function getInfo(URL) {
+  try {
+    const feedback = await fetch(URL);
+    const data = await response.json;
+    document.getElementById("recipes").textContent = data.content;
+  } catch (error) {
+    console.log(error);
+  }
+}
