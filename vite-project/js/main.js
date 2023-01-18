7a28b519049e989290fe70906697513e49795221
kevinc2465 <chenk5104@gmail.com>
2023-01-18 13:13:43 -0500

    a lot of work

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -8,15 +8,20 @@ async function getData(URL) {
     const response = await fetch(URL);
     const infos = await response.json();
     console.log(infos);
-    // infos.forEach((info) => {
-    //   DOM.recipes.insertAdjacentHTML("beforeend"`
-    //   <div class="indvidual-beer">
-    //   <h2>${info.name}</h2>
-    //   </div>
-    //   `);
-    // });
-    // DOM.recipes.innerHTML = infos.name;
-    console.log(infos[2].name);
+    infos.forEach((info) => {
+      DOM.recipes.insertAdjacentHTML(
+        "beforeend",
+        `
+          <div class="result">
+          <h2 class="beverage-name">${info.name}</h2>
+          <img class="image" src= "${info.image_url}"/>
+          <p class="paragraph">"${info.first_brewed}"</p>
+          <p class="paragraph">"${info.tagline}"</p>
+          <p class=""paragraph">"${info.description}"</p>
+          </div>
+        `
+      );
+    });
   } catch (error) {
     console.log(error);
   }
