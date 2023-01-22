051fbbaa7c5b08c6831174bbddd9b3ed5a337369
kevinc2465 <chenk5104@gmail.com>
2023-01-21 22:34:34 -0500

    added function to one button

--- vite-project/js/DOM.js
+++ vite-project/js/DOM.js
@@ -1,5 +1,10 @@
 const DOM = {
   recipes: document.getElementById("recipes"),
-};
+  attenuation: document.getElementById("attenuation-level"),
+  ph: document.getElementById("ph"),
+  SRM: document.getElementById("alcohol-srm"),
+  IBU: document.getElementById("IBU"),
+  reset: document.getElementById("reset"),
+}
 
 export { DOM };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -28,3 +28,37 @@ async function getData(URL) {
 }
 
 getData(URL);
+
+function clearField() {
+  document.querySelectorAll(".result").forEach((result) => {
+    result.remove();
+  });
+};
+
+function sortAttenuation(){
+  const results = infos.filter((info) => info.attenuation_level === 80);
+  results.forEach((info) => {
+    DOM.recipes.insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="result">
+        <h2 class="beverage-name">${info.name}</h2>
+        <img class="image" src= "${info.image_url}"/>
+        <p class="header">Year produced: ${info.first_brewed}</p>
+        <p class="header">Tagline: ${info.tagline}</p>
+        <p class="paragraph">Descriptions: ${info.description}</p>
+        </div>
+      `
+    );
+  })
+};
+
+DOM.attenuation.addEventListener("click", function () {
+  clearField();
+  sortAttenuation();
+});
+
+DOM.reset.addEventListener("click", function () {
+  clearField();
+  getData(URL);
+});
